cb7c3e254b5949eed7790985613db51af1e522ac
Dmytro Riabovol <ryabovoldima@gmail.com>
2024-03-08 09:08:04 +0200

    Setup webpack config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+};

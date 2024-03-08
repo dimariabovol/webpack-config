0d6f5b19f92ce0d50005ad998c35760621af9741
Dmytro Riabovol <ryabovoldima@gmail.com>
2024-03-08 10:20:45 +0200

    Configure babel loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import generateJoke from './generate-joke.js';
 import './scss/main.scss';
 
+const test = () => 'test';
+
+console.log(test());
 console.log(generateJoke());
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,8 @@ module.exports = {
     filename: '[name][contenthash].js',
     clean: true,
   },
+  watch: true,
+  devtool: 'source-map',
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist'),
@@ -26,6 +28,16 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
